e0cff5f501581b8c3c08b20a1b1f390e84b9cd84
François Blin <frablin@gmail.com>
2020-11-27 16:04:39 +0100

    'add position method'

--- main.js
+++ main.js
@@ -22,19 +22,40 @@ class Field {
   }
 
   input () {
-    let position = prompt('Find your hat, which way ?: ')
+    let input = prompt('Find your hat, which way ? u, d, r, l: ')
+  }
+
+  position (input) {
+    let x = 0
+    let y = 0
+    let playerPosition = [x][y]
+    if (input = 'r') {
+      y += 1
+    } else if (input === 'l') {
+      y -= 1
+    } else if (input === 'd') {
+      x += 1
+    } else if (input === 'u') {
+      x -= 1
+    } else {
+      console.log('Please enter u, d, r or l')
+    }
+    return playerPosition
   }
 
   success () {
-    while (!foundHat)
-    for (let i = 0; i < this._field.length; i++) {
-      for (let j = 0; j < this._field[i].length; j++) {
-        if (this._field[i][j] === hole || this._field[i][j] === -1) {
-          console.log(`You lost`)
-          foundHat = true
-        } else if (this._field[i][j] === hat) {
-          console.log('You won')
-          foundHat = false
+    while (!foundHat) {
+      for (let i = 0; i < this._field.length; i++) {
+        for (let j = 0; j < this._field[i].length; j++) {
+          if (this._field[i][j] === hole || this._field[i][j] === -1) {
+            console.log(`You lost`)
+            foundHat = true
+          } else if (this._field[i][j] === hat) {
+            console.log('You won')
+            foundHat = true
+          } else {
+            console.log('Continue')
+          }
         }
       }
     }
